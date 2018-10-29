faebb186915cc886cc2de162152f5c214f0ddbfa
rover95 <rover95rx@gmail.com>
2018-10-29 22:24:45 +0800

    init

---
+++ src/encrypt.js
@@ -0,0 +1,2 @@
+
+document.getElementById('box').innerHTML = wordsToMorse
---
+++ src/morse.js
@@ -0,0 +1,29 @@
+const morseWords = ['.-','-...','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','.--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..']
+
+const morseNumber = ['-----','.----','..---','...--','....-','.....',' -....',' --...','---..','----.']
+
+let wordsToMorse = {};
+let morseToWords = {};
+
+let morseToNum = {};
+let numToMorse = morseNumber;
+
+let words=[];
+for (let i = 10; i < 36; i++) {
+    let j = i.toString(36)
+    words.push(j);
+}
+//字母加密字典
+for (let i in words) {
+    wordsToMorse[words[i]] = morseWords[i];
+}
+//字母解密字典
+for (let i in wordsToMorse) {
+    morseToWords[wordsToMorse[i]] = i;
+}
+//数字解密字典
+for (let i in morseNumber){
+    morseToNum[morseNumber[i]]=i;
+}
+
+console.log(wordsToMorse, morseToWords, morseToNum);
