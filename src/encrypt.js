f5b41f8744fe6af714f7814bb9a4815ea6933ee6
rover95 <rover95rx@gmail.com>
2018-11-01 23:25:10 +0800

    demo

--- src/encrypt.js
+++ src/encrypt.js
@@ -1,2 +1,33 @@
 
-document.getElementById('box').innerHTML = wordsToMorse
+let w = 'rovelast'
+let o = []
+for(let i in w){
+    o.push(wordsToMorse[w[i]]);
+}
+let encrypt = o.join("/");
+
+let l = "&#8205;";
+let s = "&#8204;";
+let q = "&#8203;";
+
+encrypt = encrypt.replace(/\//g, q)
+encrypt = encrypt.replace(/\./g, s)
+encrypt = encrypt.replace(/\-/g, l)
+encrypt += 'im here';
+
+let decode = encrypt.match(/(\&\#8203\;|\&\#8204\;|\&\#8205\;)+/g)[0];
+console.log(decode);
+decode = decode.replace(/\&\#8203\;/g, "/");
+decode = decode.replace(/\&\#8204\;/g, ".");
+decode = decode.replace(/\&\#8205\;/g, "-");
+
+let tep = decode.split('/')
+let c = []
+for(let i in tep){
+    c.push(morseToWords[tep[i]]);
+}
+
+console.log(c);
+
+
+document.getElementById("box").innerHTML = encrypt+'im here';
