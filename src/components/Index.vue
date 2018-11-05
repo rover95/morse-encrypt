332b44ee0b9e96662ccfdfb4a3d75787ffec03b8
rover95 <rover95rx@gmail.com>
2018-11-05 23:24:29 +0800

    vue 初始化

---
+++ src/components/Index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <input type="text" id="ipt">
+    <button onclick="ddd()">加密</button>
+    <br>
+    <input type="text" id="show">
+    <button onclick="ttt();">解密</button>
+    <div id="box"></div>
+  </div>
+</template>
+
+<script>
+import { incode,decode } from "../encrypt.js";
+
+let t = incode('love', 'llll')
+
+console.log(t);
+
+let w = decode(t)
+
+console.log(w);
+function ddd() {
+    let ipt = document.getElementById("ipt").value;
+    let code = incode(ipt, "w");
+    console.log(code);
+    document.getElementById("box").innerHTML = code;
+    let t = document.getElementById("box").innerHTML
+    document.getElementById("show").value = t;
+    setTimeout(() => {
+        document.getElementById("box").innerHTML = document.getElementById("show").value;
+
+    }, 1000);
+
+}
+function ttt() {
+    let ipt = document.getElementById("show").value;
+    let code = decode(ipt);
+    console.log(code);
+    document.getElementById("box").innerHTML = code;
+}
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'encrypt'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
 import { incode, decode} from "./encrypt"
 
-let t = incode('love', 'llll')
 
-console.log(t);
-
-let w = decode(t)
-
-console.log(w);
-function ddd() {
-    let ipt = document.getElementById("ipt").value;
-    let code = incode(ipt, "w");
-    console.log(code);
-    document.getElementById("box").innerHTML = code;
-    let t = document.getElementById("box").innerHTML
-    document.getElementById("show").value = t;
-    setTimeout(() => {
-        document.getElementById("box").innerHTML = document.getElementById("show").value;
-
-    }, 1000);
-
-}
-function ttt() {
-    let ipt = document.getElementById("show").value;
-    let code = decode(ipt);
-    console.log(code);
-    document.getElementById("box").innerHTML = code;
-}
 
 let app = `<div>
     <input type="text" id="ipt">
