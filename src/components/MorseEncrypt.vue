906d79c4f89a8a69f48f3385ac5776ea33ed467d
rover95 <rover95rx@gmail.com>
2018-11-06 22:00:34 +0800

    *

---
+++ src/components/MorseEncrypt.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input v-model="incodes">
+    <button @click="handleIncode">加密</button>
+    <br>
+    <input id="output" v-model="outcodes">
+    <button @click="handleDecode">解密</button>
+    <div id="box">{{outcodes}}</div>
+  </div>
+</template>
+
+<script>
+import { incode, decode } from "../encrypt.js";
+
+export default {
+  name: "Index",
+  data() {
+    return {
+      msg: "encrypt",
+      incodes: "",
+      outcodes: ""
+    };
+  },
+  created: function() {},
+  methods: {
+    handleIncode: function() {
+      let code = incode(this.incodes, "w");
+      this.outcodes = code;
+      console.log(code);
+      document.getElementById("output").value = code;
+      document.getElementById("box").innerHTML = code;
+    },
+    handleDecode: function() {
+      let code = decode(this.outcodes);
+      console.log(code);
+      this.outcodes = code;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/components/Index'
+import MorseEncrypt from '@/components/MorseEncrypt'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'index',
-      component: Index
+      name: 'MorseEncrypt',
+      component: MorseEncrypt
     }
   ]
-})
+});
